32250e2ab06d2121de15616b2e62874dadc0ad06
darwinQSot <quispesotodaniel@gmail.com>
2022-08-05 22:42:01 -0500

    S2: Microservicio Usuarios - Implementando el controlador RestController y metodos handler GET

---
+++ curso-kubernetes/msvc-usuarios/src/main/java/org/dardan/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -0,0 +1,33 @@
+package org.dardan.springcloud.msvc.usuarios.controllers;
+
+import org.dardan.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.dardan.springcloud.msvc.usuarios.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if (usuarioOptional.isPresent()) {
+            return ResponseEntity.ok(usuarioOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
