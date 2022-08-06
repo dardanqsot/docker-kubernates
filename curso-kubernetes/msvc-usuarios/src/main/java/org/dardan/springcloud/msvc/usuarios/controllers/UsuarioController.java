65e54c83916cd33fb60b5faa65f345373207da5b
darwinQSot <quispesotodaniel@gmail.com>
2022-08-05 22:50:20 -0500

    S2: Microservicio Usuarios - RestController y metodos handler DELETE

--- curso-kubernetes/msvc-usuarios/src/main/java/org/dardan/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ curso-kubernetes/msvc-usuarios/src/main/java/org/dardan/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -48,4 +48,13 @@ public class UsuarioController {
         return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        Optional<Usuario> o = service.porId(id);
+        if (o.isPresent()) {
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
