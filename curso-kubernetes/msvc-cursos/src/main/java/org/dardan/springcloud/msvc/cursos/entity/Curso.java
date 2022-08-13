988bca01b7e7ad1d9dd52f05cd4a8aec62cf74fe
darwinQSot <quispesotodaniel@gmail.com>
2022-08-12 22:41:15 -0500

    S5: Cliente HTTP Feign Comunicación entre microservicios - Creando JPA entity intermedia CursoUsuario

--- curso-kubernetes/msvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/entity/Curso.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/entity/Curso.java
@@ -2,6 +2,8 @@ package org.dardan.springcloud.msvc.cursos.entity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "cursos")
@@ -13,6 +15,13 @@ public class Curso {
 
     @NotEmpty
     private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CursoUsuario> cursoUsuarios;
+
+    public Curso() {
+        cursoUsuarios = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -28,5 +37,20 @@ public class Curso {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void addCursoUsuario(CursoUsuario cursoUsuario) {
+        cursoUsuarios.add(cursoUsuario);
+    }
+
+    public void removeCursoUsuario(CursoUsuario cursoUsuario) {
+        cursoUsuarios.remove(cursoUsuario);
+    }
+    public List<CursoUsuario> getCursoUsuarios() {
+        return cursoUsuarios;
+    }
+
+    public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
+        this.cursoUsuarios = cursoUsuarios;
     }
 }
