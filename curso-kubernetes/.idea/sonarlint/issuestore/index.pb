f98f5cd3d9b6bcbdc9a18bc54fcc67e0adc97098
darwinQSot <quispesotodaniel@gmail.com>
2022-08-21 16:31:44 -0500

    S5: Cliente HTTP Feign Comunicación entre microservicios - Añadiendo métodos de comunicación en el Controlador Rest.

--- curso-kubernetes/.idea/sonarlint/issuestore/index.pb
+++ curso-kubernetes/.idea/sonarlint/issuestore/index.pb
@@ -31,8 +31,6 @@ Pmsvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/models/Usuario.jav
 cmsvc-usuarios/src/main/java/org/dardan/springcloud/msvc/usuarios/controllers/UsuarioController.java,a\7\a7ac5e5904c2dcc84d0411d384c7f7061589b017
 á
 Wmsvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/services/CursoService.java,7\4\744c8a2a6e5c66ada50ef08971655b292b54d3b8
-ç
-]msvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/controllers/CursoController.java,a\7\a716317fb399dfda553c17179634cd275292690a
 Ö
 Umsvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/models/entity/Curso.java,6\4\643e11f99fb9cb6f2b2cc43813dbc4f811722b6b
 å
--- curso-kubernetes/msvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/controllers/CursoController.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/dardan/springcloud/msvc/cursos/controllers/CursoController.java
@@ -1,5 +1,7 @@
 package org.dardan.springcloud.msvc.cursos.controllers;
 
+import feign.FeignException;
+import org.dardan.springcloud.msvc.cursos.models.Usuario;
 import org.dardan.springcloud.msvc.cursos.models.entity.Curso;
 import org.dardan.springcloud.msvc.cursos.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class CursoController {
@@ -32,6 +31,54 @@ public class CursoController {
             return ResponseEntity.ok(o.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/asignar-usuario/{cursoId}")
+    public ResponseEntity<?> asignarUsuario(@RequestBody Usuario usuario, @PathVariable Long cursoId) {
+        Optional<Usuario> o;
+        try{
+            o = service.asignarUsuario(usuario, cursoId);
+        } catch (FeignException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).
+                    body(Collections.singletonMap( "mensaje", "No existe el usuario por" +
+                            " el id o error en la comunicación: " + e.getMessage()));
+        }
+        if(o.isPresent()){
+            return ResponseEntity.status(HttpStatus.CREATED).body(o.get());
+        }
+        return  ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/crear-usuario/{cursoId}")
+    public ResponseEntity<?> crearUsuario(@RequestBody Usuario usuario, @PathVariable Long cursoId) {
+        Optional<Usuario> o;
+        try{
+            o = service.crearUsuario(usuario, cursoId);
+        } catch (FeignException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).
+                    body(Collections.singletonMap( "mensaje", "No se pudo crear el usuario" +
+                            " o error en la comunicación: " + e.getMessage()));
+        }
+        if(o.isPresent()){
+            return ResponseEntity.status(HttpStatus.CREATED).body(o.get());
+        }
+        return  ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/eliminar-usuario/{cursoId}")
+    public ResponseEntity<?> eliminarUsuario(@RequestBody Usuario usuario, @PathVariable Long cursoId) {
+        Optional<Usuario> o;
+        try{
+            o = service.eliminarUsuario(usuario, cursoId);
+        } catch (FeignException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).
+                    body(Collections.singletonMap( "mensaje", "No se pudo eliminar usuario por" +
+                            " el id o error en la comunicación: " + e.getMessage()));
+        }
+        if(o.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(o.get());
+        }
+        return  ResponseEntity.notFound().build();
     }
 
     @PostMapping("/")
